8a87ca8244ee80b4da019fa106e6ea4e5837207f
franciscaserrao <francisca.s.serrao@gmail.com>
2024-03-31 22:20:14 +0100

    Created basic search engine

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import "./App.css";
+import Dictionary from "./Dictionary";
+
+function App() {
+  return (
+    <div className="App">
+      <div className="container">
+        <header className="App-header">Hello </header>
+        <main>
+          <Dictionary />
+        </main>
+        <footer className="App-footer">Coded by SheCodes</footer>
+      </div>
+    </div>
+  );
+}
+
+export default App;
