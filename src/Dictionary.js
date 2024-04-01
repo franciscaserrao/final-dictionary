dfdc78c3c10bb31da3de833a06124f543e940677
franciscaserrao <francisca.s.serrao@gmail.com>
2024-04-01 09:51:24 +0100

    Added dictionary api integration

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,12 +1,20 @@
 import React, { useState } from "react";
+import axios from "axios";
 import "./Dictionary.css";
 
 export default function Dictionary() {
   let [keyword, setKeyword] = useState("");
 
+  function handleResponse(response) {
+    console.log(response.data);
+  }
+
   function search(event) {
     event.preventDefault();
     alert(`Searching for ${keyword}`);
+    let apiKey = "9tf8b32027ebcbc4fod15cda0efbf2e3";
+    let apiUrl = `https://api.shecodes.io/dictionary/v1/define?word=${keyword}&key=${apiKey}`;
+    axios.get(apiUrl).then(handleResponse);
   }
 
   function handleKeywordChange(event) {
