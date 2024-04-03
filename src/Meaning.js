a70b388a8ce7424bd884f8be1bdaaa06fb3ea35d
franciscaserrao <francisca.s.serrao@gmail.com>
2024-04-03 15:10:08 +0100

    Created Synonyms variable

--- src/Meaning.js
+++ src/Meaning.js
@@ -1,13 +1,18 @@
 import React from "react";
+import Synonyms from "./Synonyms";
 
 export default function Meaning(props) {
   return (
     <div className="Meaning">
       <h3>{props.meaning.partOfSpeech}</h3>
-      <p>{props.meaning.definition}</p>
-      <em>
-        <p>{props.meaning.example}</p>
-      </em>
+      <p>
+        <strong>Definition: </strong>
+        {props.meaning.definition}
+        <br />
+        <strong>Example: </strong>
+        <em>{props.meaning.example}</em>
+        <Synonyms synonyms={props.meaning.synonyms} />
+      </p>
     </div>
   );
 }
