205984a8fe9d232149c8eaa180d0f62bc9d7fa1e
franciscaserrao <francisca.s.serrao@gmail.com>
2024-04-04 18:00:31 +0100

    Updated footer

--- src/App.js
+++ src/App.js
@@ -11,7 +11,32 @@ function App() {
         <main>
           <Dictionary defaultKeyword="sunset" />
         </main>
-        <footer className="App-footer">Coded by SheCodes</footer>
+        <footer className="App-footer">
+          Coded by{" "}
+          <a
+            href="https://github.com/franciscaserrao"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Francisca Serrão
+          </a>
+          , open-sourced on{" "}
+          <a
+            href="https://github.com/franciscaserrao/final-dictionary"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Github
+          </a>{" "}
+          and hosted on{" "}
+          <a
+            href="https://main--final-dictionary-shecodes-react-app.netlify.app/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Netlify
+          </a>
+        </footer>
       </div>
     </div>
   );
